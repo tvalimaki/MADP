5a799afd9f49701d8d8893977325ddf6194eed3f
Tuomas Välimäki <tuomas.s.valimaki@tut.fi>
2018-07-05 11:13:19 +0300

    use arbitrary precision

--- src/include/configuration.h
+++ src/include/configuration.h
@@ -1,14 +1,14 @@
-/* This file is part of the Multiagent Decision Process (MADP) Toolbox. 
+/* This file is part of the Multiagent Decision Process (MADP) Toolbox.
  *
  * The majority of MADP is free software released under GNUP GPL v.3. However,
- * some of the included libraries are released under a different license. For 
- * more information, see the included COPYING file. For other information, 
+ * some of the included libraries are released under a different license. For
+ * more information, see the included COPYING file. For other information,
  * please refer to the included README file.
  *
  * This file has been written and/or modified by the following people:
  *
- * Frans Oliehoek 
- * Matthijs Spaan 
+ * Frans Oliehoek
+ * Matthijs Spaan
  *
  * For contact information please see the included AUTHORS file.
  */
@@ -18,7 +18,7 @@
 // usually also requires modifying Makefile.custom.
 
 // Indicates whether we use libgmp to represent LIndex.
-#define USE_ARBITRARY_PRECISION_INDEX 0
+#define USE_ARBITRARY_PRECISION_INDEX 1
 
 // Indicates whether to use the pomdp-solve library, or call an external program.
 //  #if !DARWIN
